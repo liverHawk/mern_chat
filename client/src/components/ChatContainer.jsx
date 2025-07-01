7d2339b996d759f29f024c7cab2589428a26a311
liverHawk <hawkyofkn5365@gmail.com>
2025-07-01 20:13:41 +0900

    chat tmp done

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -33,7 +33,7 @@ export default function ChatContainer({ selectedUser, setSelectedUser }) {
                 <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
                     {
                         messagesDummyData.map((msg, index) => (
-                            <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== "680f5116f10f3cd28382ed02" && "flex-row-reverse"}`}>
+                            <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== "680f50e4f10f3cd28382ecf9" && "flex-row-reverse"}`}>
                                 {
                                     msg.image ? (
                                         <img 
@@ -44,7 +44,7 @@ export default function ChatContainer({ selectedUser, setSelectedUser }) {
                                     ) : (
                                         <p className={`
                                             p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white
-                                            ${msg.senderId === "680f50e4f10f3cd28382ecf9" ? "rounded-br-none" : "rounded-bl-none"}
+                                            ${msg.senderId === "680f50e4f10f3cd28382ecf9" ? "rounded-br-none" : ""}
                                         `}>
                                             {msg.text}
                                         </p>
@@ -52,7 +52,7 @@ export default function ChatContainer({ selectedUser, setSelectedUser }) {
                                 }
                                 <div className="text-center text-xs">
                                     <img 
-                                        src={msg.senderId === "680f5116f10f3cd28382ed02" ? assets.avatar_icon : assets.profile_martin}
+                                        src={msg.senderId === "680f50e4f10f3cd28382ecf9" ? assets.avatar_icon : assets.profile_martin}
                                         alt="" 
                                         className="w-7 rounded-full"
                                     />
@@ -64,6 +64,21 @@ export default function ChatContainer({ selectedUser, setSelectedUser }) {
                         ))
                     }
                     <div ref={scrollEnd}></div>
+                </div>
+                {/* bottom area */}
+                <div className="absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3">
+                    <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full">
+                        <input 
+                            type="text" 
+                            placeholder="Send a message"
+                            className="flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400" 
+                        />
+                        <input type="file" id="image" accept="image/png,image/jpeg" hidden/>
+                        <label htmlFor="image">
+                            <img src={assets.gallery_icon} alt="" className="w-5 mr-2 cursor-pointer" />
+                        </label>
+                    </div>
+                    <img src={assets.send_button} alt="" className="w-7 cursor-pointer" />
                 </div>
             </div>
         )
