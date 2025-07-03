362bbef35d1be15ad6b373c4985ca1557fe549a9
liverHawk <hawkyofkn5365@gmail.com>
2025-07-03 14:01:51 +0900

    base files done

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import "dotenv/config";
+import cors from 'cors';
+import http from "http";
+
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json({ limit: '4mb' }));
+app.use(cors());
+
+app.use(
+    "/api/status",
+    (req, res) => {
+        res.status(200).json({ status: "Server is running" });
+    }
+)
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
