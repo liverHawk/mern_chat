b9b874dc8eec96929d234a2d8ed046eea6b2dd22
liverHawk <hawkyofkn5365@gmail.com>
2025-07-03 13:08:40 +0900

    loginpage

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -9,13 +9,34 @@ export default function LoginPage() {
     const [bio, setBio] = useState("");
     const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        if (currentState === "Sign up" && !isDataSubmitted) {
+            setIsDataSubmitted(true);
+            return;
+        }
+    }
+
     return (
         <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
             <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]'/>
-            <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+            <form 
+                onSubmit={onSubmitHandler}
+                className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'
+            >
                 <h2 className='font-medium text-2xl flex justify-between items-center'>
                     {currentState}
-                    <img src={assets.arrow_icon} alt="" className='w-5 cursor-pointer'/>
+                    {isDataSubmitted && (
+                        <img
+                            onClick={() => {
+                                setIsDataSubmitted(false);
+                            }}
+                            src={assets.arrow_icon} 
+                            alt="" 
+                            className='w-5 cursor-pointer'
+                        />
+                    )}
                 </h2>
                 {
                     currentState === "Sign up" && !isDataSubmitted && (
@@ -72,8 +93,41 @@ export default function LoginPage() {
 
                 <div className='flex items-center gap-2 text-sm text-gray-500'>
                     <input type="checkbox" />
-                    <p>Agree to the terms of use & privacy policy.
+                    <p>
+                        Agree to the terms of use & privacy policy.
                     </p>
+                </div>
+
+                <div className='flex flex-col gap-2'>
+                    {
+                        currentState === "Sign up" ? (
+                            <p className='text-sm text-gray-600'>
+                                Already have an account? 
+                                <span
+                                    onClick={() => {
+                                        setCurrentState("Login");
+                                        setIsDataSubmitted(false);
+                                    }}
+                                    className='ml-4 text-medium text-violet-500 cursor-pointer'
+                                >
+                                    Login here
+                                </span>
+                            </p>
+                        ) : (
+                            <p className='text-sm text-gray-600'>
+                                Create an account 
+                                <span
+                                    onClick={() => {
+                                        setCurrentState("Sign up");
+                                        setIsDataSubmitted(false);
+                                    }}
+                                    className='ml-4 text-medium text-violet-500 cursor-pointer'
+                                >
+                                    Sign up here
+                                </span>
+                            </p>
+                        )
+                    }
                 </div>
             </form>
         </div>
